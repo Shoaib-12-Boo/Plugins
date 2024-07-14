4fe7db7d143108536d9243143b9809de6a948ecb
M.Shoaib Shafique <72503428+Shoaib-12-Boo@users.noreply.github.com>
2024-07-14 10:40:47 +0500

    Update New Text Document.php

--- Food pack/New Text Document.php
+++ Food pack/New Text Document.php
@@ -1,124 +1,68 @@
 <?php
-
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              https://techosolution.com
- * @since             1.0.0
- * @package           Meal_Customization
- *
- * @wordpress-plugin
- * Plugin Name:       Meal Customization
- * Plugin URI:        https://techosolution.com
- * Description:       Meal Customization
- * Version:           1.0.0
- * Author:            Shoaib
- * Author URI:        https://techosolution.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       meal-customization
- * Domain Path:       /languages
+ * @package Akismet
  */
+/*
+Plugin Name: Akismet Anti-spam: Spam Protection
+Plugin URI: https://akismet.com/
+Description: Used by millions, Akismet is quite possibly the best way in the world to <strong>protect your blog from spam</strong>. Akismet Anti-spam keeps your site protected even while you sleep. To get started: activate the Akismet plugin and then go to your Akismet Settings page to set up your API key.
+Version: 5.3.3
+Requires at least: 5.8
+Requires PHP: 5.6.20
+Author: Automattic - Anti-spam Team
+Author URI: https://automattic.com/wordpress-plugins/
+License: GPLv2 or later
+Text Domain: akismet
+*/
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2023 Automattic, Inc.
+*/
+
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'MEAL_CUSTOMIZATION_VERSION', '1.0.0' );
+define( 'AKISMET_VERSION', '5.3.3' );
+define( 'AKISMET__MINIMUM_WP_VERSION', '5.8' );
+define( 'AKISMET__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'AKISMET_DELETE_LIMIT', 10000 );
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-meal-customization-activator.php
- */
-function activate_meal_customization() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-meal-customization-activator.php';
-	Meal_Customization_Activator::activate();
+register_activation_hook( __FILE__, array( 'Akismet', 'plugin_activation' ) );
+register_deactivation_hook( __FILE__, array( 'Akismet', 'plugin_deactivation' ) );
+
+require_once AKISMET__PLUGIN_DIR . 'class.akismet.php';
+require_once AKISMET__PLUGIN_DIR . 'class.akismet-widget.php';
+require_once AKISMET__PLUGIN_DIR . 'class.akismet-rest-api.php';
+
+add_action( 'init', array( 'Akismet', 'init' ) );
+
+add_action( 'rest_api_init', array( 'Akismet_REST_API', 'init' ) );
+
+if ( is_admin() || ( defined( 'WP_CLI' ) && WP_CLI ) ) {
+	require_once AKISMET__PLUGIN_DIR . 'class.akismet-admin.php';
+	add_action( 'init', array( 'Akismet_Admin', 'init' ) );
 }
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-meal-customization-deactivator.php
- */
-function deactivate_meal_customization() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-meal-customization-deactivator.php';
-	Meal_Customization_Deactivator::deactivate();
-}
+//add wrapper class around deprecated akismet functions that are referenced elsewhere
+require_once AKISMET__PLUGIN_DIR . 'wrapper.php';
 
-register_activation_hook( __FILE__, 'activate_meal_customization' );
-register_deactivation_hook( __FILE__, 'deactivate_meal_customization' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-meal-customization.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_meal_customization() {
-    $plugin = new Meal_Customization();
-    $plugin->run();
-}
-run_meal_customization();
-
-add_action('woocommerce_before_cart', 'check_cart_quantities');
-
-function check_cart_quantities() {
-    $cart_items = WC()->cart->get_cart();
-    $pack_quantity = 0;
-    $cart_quantity = 0;
-
-    foreach ($cart_items as $cart_item_key => $cart_item) {
-        if (isset($cart_item['wsf_item_data']) && !empty($cart_item['wsf_item_data'])) {
-            foreach ($cart_item['wsf_item_data'] as $meta_data) {
-                if (isset($meta_data['key']) && $meta_data['key'] === 'Select your pack') {
-                    $pack_quantity = intval($meta_data['value']);
-                }
-            }
-        }
-        $cart_quantity += $cart_item['quantity'];
-    }
-
-    if ($cart_quantity < $pack_quantity) {
-        wc_print_notice('Please complete your food pack.', 'error');
-        add_action('wp_footer', 'disable_checkout_button');
-    } elseif ($cart_quantity > $pack_quantity) {
-        wc_print_notice('Please Select Another food pack.', 'error');
-        add_action('wp_footer', 'disable_checkout_button');
-    }
-}
-function disable_checkout_button() {
-    ?>
-    <style>
-        ul.woocommerce-error {
-    width: 100%;
-}
-    </style>
-    <script type="text/javascript">
-        jQuery(document).ready(function($) {
-            var checkoutButton = $('.checkout-button');
-            checkoutButton.addClass('disabled').click(function(event) {
-                event.preventDefault();
-            });
-        });
-    </script>
-    <?php
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	require_once AKISMET__PLUGIN_DIR . 'class.akismet-cli.php';
 }
